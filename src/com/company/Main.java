bd63c3404ccffcc2852fad9d52426886744f939e
Zigmas <zambrazaitis@gmail.com>
2018-01-30 15:13:00 +0200

    uzduotis03

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    // 1.3.​ Parašykite programą, kuri paprašytų įvesti vartotojo du skaičius ir atspausdinti jų
+        // skirtumą bei sumą
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Iveskite du skaicius: ");
+        int sk1 = sc.nextInt();
+        int sk2 = sc.nextInt();
+        int suma = sk1 + sk2;
+        int skirtumas = sk1 - sk2;
+        System.out.println("Suma= " + suma);
+        System.out.println("Skirtumas= " + skirtumas);
     }
 }
